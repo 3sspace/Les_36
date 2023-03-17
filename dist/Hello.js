407e8a3c61ff6ea39e09b8161d21284b6798daf1
3sspace <vinudbe@ukr.net>
2023-03-17 16:22:11 +0200

    babelreact

--- dist/Hello.js
+++ dist/Hello.js
@@ -1,9 +1,8 @@
 function Hello() {
-  return React.createElement(
-      "h2",
-      null,
-      "Hello React"
-  );
+    return React.createElement(
+        "h2",
+        null,
+        "Hello React"
+    );
 }
-
 export default Hello;
